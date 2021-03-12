3f92ca702786ed1b0b4f678dea804d0137f04f34
Seth Lewandowski <sethlew@protonmail.com>
2021-03-12 11:13:45 -0700

    let & const

---
+++ Section2/letAndConst.js
@@ -0,0 +1,13 @@
+// let & const
+// ways to create variables
+// es6 introduced let & const instead of var
+// let is new var
+// const for constants (more often than you think)
+// let is if you want a variable
+
+// const will throw error if you reassign value
+
+const myName = "seth"
+
+myName = "jordan" // throws assignment to constant variable error
+console.log(myName)

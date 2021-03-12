d440f817c9db78cae6eed5c545ca2671b9e6cfc1
Seth Lewandowski <sethlew@protonmail.com>
2021-03-12 13:08:38 -0700

    36 finished

--- Section3/react-complete-guide/src/App.js
+++ Section3/react-complete-guide/src/App.js
@@ -7,10 +7,11 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>Hi I'm a react app</h1>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <Person name='seth' age='28' />
+        <Person name='stephanie' age='30'>
+          MyHobbies: racing
+        </Person>
+        <Person name='ted' age='5' />
       </div>
     )
   }

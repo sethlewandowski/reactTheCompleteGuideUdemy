211d73f17262f05f347628e7e7762d0642886158
Seth Lewandowski <sethlew@protonmail.com>
2021-03-12 11:27:48 -0700

    new way of using classes, props, and methods

---
+++ Section2/classesPropsAndMethods.js
@@ -0,0 +1,19 @@
+// props are like variables attached to classes/objs
+// methods are like functions attached to classes/objs
+// es7 allows us to skip constructor function call
+
+
+// old way
+constructor(){
+    this.myProp = 'value'
+}
+
+//new way
+myProperty = 'value'
+
+
+// old way
+myMethod(){...}
+
+// new way
+myMethod = () => {...}

ad5d6316db97420996fefcc734ebd3e9974ececc
Seth Lewandowski <sethlew@protonmail.com>
2021-03-12 11:49:18 -0700

    arrayfuncs

--- Section2/arrayFunctions.js
+++ Section2/arrayFunctions.js
@@ -1,4 +1,6 @@
 const numbers = [1, 2, 3]
 
-const doubleNumArr = numbers.map((x) => x * 2)
+const doubleNumArr = numbers.map((x) => x * 2) // map returns new arr
+
+console.log(numbers)
 console.log(doubleNumArr)

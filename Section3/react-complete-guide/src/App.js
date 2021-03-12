8d362109155a1af34c9a9bff58a945a189eeaa83
Seth Lewandowski <sethlew@protonmail.com>
2021-03-12 13:16:48 -0700

    props vs state

--- Section3/react-complete-guide/src/App.js
+++ Section3/react-complete-guide/src/App.js
@@ -3,15 +3,42 @@ import "./App.css"
 import Person from "./Person/Person"
 
 class App extends Component {
+  // only works in components
+  state = {
+    persons: [
+      {
+        name: "seth",
+        age: 28,
+      },
+      {
+        name: "ted",
+        age: 33,
+      },
+      {
+        name: "emily",
+        age: 2,
+      },
+    ],
+  }
+
   render() {
     return (
       <div className='App'>
         <h1>Hi I'm a react app</h1>
-        <Person name='seth' age='28' />
-        <Person name='stephanie' age='30'>
-          MyHobbies: racing
-        </Person>
-        <Person name='ted' age='5' />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+
+        <button>Switch Name</button>
       </div>
     )
   }

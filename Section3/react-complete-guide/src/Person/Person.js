fd9de367537718853450a5d0cdf04b99d7f2f258
Seth Lewandowski <sethlew@protonmail.com>
2021-03-13 09:12:50 -0700

    finished section

--- Section3/react-complete-guide/src/Person/Person.js
+++ Section3/react-complete-guide/src/Person/Person.js
@@ -1,16 +1,5 @@
 import React from "react"
 import "./Person.css"
-import styled from "styled-components"
-
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-  margin: auto;
-`
 
 const person = (props) => {
   const style = {
@@ -20,14 +9,13 @@ const person = (props) => {
   }
 
   return (
-    <StyledDiv>
+    <div style={style}>
       <p onClick={props.click}>
         I'm {props.name}! I am {props.age} years old! Click to delete.
       </p>
       <p>{props.children}</p>
       <input type='text' onChange={props.changed} value={props.name} />
-    </StyledDiv>
-    //<div className='Person' style={style}>
+    </div>
   )
 }
 
